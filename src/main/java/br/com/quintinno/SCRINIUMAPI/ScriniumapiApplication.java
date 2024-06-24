3460a83ceba3ec85b003cd5da55d846324c8c3e6
José Quintino <jose.quintino@fornecedores.sicoob.com.br>
2024-06-24 12:16:01 -0300

    ::.. SCRINIUMAPI ..::

---
+++ src/main/java/br/com/quintinno/SCRINIUMAPI/ScriniumapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.quintinno.SCRINIUMAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriniumapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriniumapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/quintinno/SCRINIUMAPI/ScriniumapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quintinno.SCRINIUMAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriniumapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
